b5fc1e914fd7209ba5408140ec1d0a957b8b909b
Kevin Costain <kevin@cwl.cc>
2013-02-10 20:06:33 -0500

    New Project - Book Reader

--- FBReader/tools/chocolateyInstall.ps1
+++ FBReader/tools/chocolateyInstall.ps1
@@ -1,17 +1,17 @@
 ﻿#NOTE: Please remove any commented lines to tidy up prior to releasing the package, including this one
 
 $packageName = 'FBReader' # arbitrary name for the package, used in messages
-$installerType = 'EXE_OR_MSI' #only one of these two: exe or msi
-$url = 'URL_HERE' # download url
+$installerType = 'EXE' #only one of these two: exe or msi
+$url = 'http://fbreader.org/files/win32/FBReaderSetup-0.12.10.exe' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
-$silentArgs = 'SILENT_ARGS_HERE' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
+$silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
 
 # main helpers - these have error handling tucked into them already
 # installer, will assert administrative rights
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
 # download and unpack a zip file
-Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
+# Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "$url64"
 
 #try { #error handling is only necessary if you need to do anything in addition to/instead of the main helpers
   # other helpers - using any of these means you want to uncomment the error handling up top and at bottom.
